72fe188eba2fe1d99b5ee1f40cc27affaf97669b
nkaslin <kaeslin.nando@gmail.com>
2021-06-17 12:58:20 +0200

    added TTFT, n-Pavlov not possible/hard to implement

--- Strategies.py
+++ Strategies.py
@@ -389,4 +389,58 @@ class ImpTFT(Strategy):
         return decision, utility
 
 
+class TTFT(Strategy):
+
+    def __init__(self) -> None:
+        """ Two Tits for Tat: Defects twice after being defected against, otherwise cooperates.
+        """
+        self.name = "TTFT"
+        super().__init__()
+
+    def make_move(self, player_one, player_two, game_config: Dict) -> Tuple[int, float]:
+        # Defect if one of the last two moves of the opponent were to defect
+        previous_games = player_one.history.game_list(player_two.id)
+        if len(previous_games) < 2:
+            decision =  0
+        elif previous_games[-1].opponent_decision == 1 or previous_games[-2].opponent_decision == 1:
+            decision =  1
+        else:
+            decision =  0
+
+        # utility not implemented
+        utility = None
+
+        return decision, utility
+    
+    
+# class nPavlov(Strategy):
+
+#     def __init__(self, n) -> None:
+#         """ n-Pavlov
+#         """
+#         self.name = "nPavlov"
+#         self.n = n
+#         super().__init__()
+
+#     def make_move(self, player_one, player_two, game_config: Dict) -> Tuple[int, float]:
+#         previous_games_one = player_one.history.game_list(player_one.id)
+#         previous_games_two = player_one.history.game_list(player_two.id)
+#         action_one = previous_games_one[-1]
+#         action_two = previous_games_two[-1]
+#         if len(previous_games_one) < 1:
+#             decision =  0
+#         else:
+#             r = random()
+#             if action_one + action_two == 2:
+                
+#         elif previous_games[-1].opponent_decision == 1 or previous_games[-2].opponent_decision == 1:
+#             decision =  1
+#         else:
+#             decision =  0
+
+#         # utility not implemented
+#         utility = None
+
+#         return decision, utility
+
 
