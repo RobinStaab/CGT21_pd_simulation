cbd986c706ed8b7aae7c5b37d1675e1a1d2465c8
Fabrice Egger <fabrice.egger@tbwil.ch>
2021-06-19 14:54:59 +0200

    finished styling

--- test_simulator.py
+++ test_simulator.py
@@ -34,7 +34,7 @@ def generate_player(strategy: Strategy, player_class: str, play_window: int, mig
         strategy_obj = CHAMPION()
     else:
         assert False, "Unkown strategy type"
-    
+
     return {
         "class": player_class,
         "play_window": play_window,
@@ -45,51 +45,58 @@ def generate_player(strategy: Strategy, player_class: str, play_window: int, mig
         "omega": omega
     }
 
+
 def generate_simple_players(strategies: List[str], amount: List[int], play_window: int, migrate_window: int, imit_prob: float, migrate_prob: float, omega: float):
     player_cfgs = []
     assert len(strategies) == len(amount)
     for amount, strategy in zip(amount, strategies):
         for i in range(amount):
-            player_cfgs.append(generate_player(strategy, strategy, play_window, migrate_window, imit_prob, migrate_prob, omega))
+            player_cfgs.append(generate_player(
+                strategy, strategy, play_window, migrate_window, imit_prob, migrate_prob, omega))
 
     return player_cfgs
 
+
 if __name__ == "__main__":
-    
+
     # T > R > P > S
     T = 1.5
     R = 1
     S = 0.5
     P = 0.8
-    grid_x          = 10
-    grid_y          = 10
-    num_players     = 100
-    play_window     = 1
-    migrate_window  = 3
-    imit_prob       = 0.8
-    migrate_prob    = 0.8
-    epochs          = 100
-    omega          = 0.5
-    
-    
-    # player_cfgs = generate_players(["GTFT","ImpTFT","TTFT","EARTHERLY","CHAMPION"], 
+    grid_x = 10
+    grid_y = 10
+    num_players = 100
+    play_window = 1
+    migrate_window = 3
+    imit_prob = 0.8
+    migrate_prob = 0.8
+    epochs = 100
+    omega = 0.5
+
+    # player_cfgs = generate_players(["GTFT","ImpTFT","TTFT","EARTHERLY","CHAMPION"],
     #                                 [40, 10, 10, 20, 20], play_window, migrate_window, imit_prob, migrate_prob, omega)
-    
-    player_cfgs = generate_simple_players(["RANDOM","DEFECT","COOPERATE","GT","TFT","TFTD","TF2T"], 
-                                    [30, 10, 10, 10, 10, 10, 10], play_window, migrate_window, imit_prob, migrate_prob, omega)
 
-    sim = Simulator(grid_x, grid_y, num_players, play_window, migrate_window, player_cfgs, T, R, S, P)
+    player_cfgs = generate_simple_players(["RANDOM", "DEFECT", "COOPERATE", "GT", "TFT", "TFTD", "TF2T"],
+                                          [30, 10, 10, 10, 10, 10, 10], play_window, migrate_window, imit_prob, migrate_prob, omega)
 
-   
+    sim = Simulator(grid_x, grid_y, num_players, play_window,
+                    migrate_window, player_cfgs, T, R, S, P)
+
     for i in range(10):
         start_time = time.time()
         sim.simulate(epochs, visualize=False)
-        t = defection_per_class_over_time(sim.get_state(), ["RANDOM","DEFECT","COOPERATE","GT","TFT","TFTD","TF2T"] )
-        t3 = class_vs_class_over_time(sim.get_state(), ["RANDOM","DEFECT","COOPERATE","GT","TFT","TFTD","TF2T"] )
-        t4 = payoff_per_class_over_time(sim.get_state(), ["RANDOM","DEFECT","COOPERATE","GT","TFT","TFTD","TF2T"] )
-        t5 = percentage_of_optimum(sim.get_state(), ["RANDOM","DEFECT","COOPERATE","GT","TFT","TFTD","TF2T"] )
+        t = defection_per_class_over_time(
+            sim.get_state(), ["RANDOM", "DEFECT", "COOPERATE", "GT", "TFT", "TFTD", "TF2T"])
+        t3 = class_vs_class_over_time(
+            sim.get_state(), ["RANDOM", "DEFECT", "COOPERATE", "GT", "TFT", "TFTD", "TF2T"])
+        t4 = payoff_per_class_over_time(
+            sim.get_state(), ["RANDOM", "DEFECT", "COOPERATE", "GT", "TFT", "TFTD", "TF2T"])
+        t5 = percentage_of_optimum(
+            sim.get_state(), ["RANDOM", "DEFECT", "COOPERATE", "GT", "TFT", "TFTD", "TF2T"])
         #t2 = class_distribution_over_time(sim.get_flat_mapped_grid(), ["RANDOM","DEFECT","COOPERATE","GT","TFT","TFTD","TF2T"] )
         #t6 = class_change_over_time
         print(f"Time: {(time.time() - start_time)}")
 
-    print(f"Total Time for {epochs} epochs, {num_players} players and grid-size {grid_x} x {grid_y}: {(time.time() - start_time)}")
+    print(
+        f"Total Time for {epochs} epochs, {num_players} players and grid-size {grid_x} x {grid_y}: {(time.time() - start_time)}")
