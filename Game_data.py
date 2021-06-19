099b9232b0207f94f72960871eb987d3cf1e73a6
Jan Urech <jurech@student.ethz.ch>
2021-06-19 12:15:43 +0200

    adding experiments

--- Game_data.py
+++ Game_data.py
@@ -1,11 +1,13 @@
 class Game_data:
-    def __init__(self, epoch, player_id, opponent_id, player_decision, opponent_decision, player_strategy, other_strategy, player_util, opponent_util, data) -> None:
+    def __init__(self, epoch, player_id, opponent_id, player_class, opponent_class, player_decision, opponent_decision, player_strategy, other_strategy, player_util, opponent_util, data) -> None:
         """Game data
 
         Args:
             epoch (int)             : epoch of the game
             player_id (int)         : id of the player
             opponent_id (int)       : id of the opponent
+            player_class (str)      : class of the player
+            opponent_class (str)    : class of the opponent
             player_decision         : decision of the player
             opponent_decision       : decision of the opponent
             player_strategy         : strategy the player is currently following
@@ -17,6 +19,8 @@ class Game_data:
         self.epoch              = epoch
         self.player_id          = player_id
         self.opponent_id        = opponent_id
+        self.player_class       = player_class
+        self.opponent_class     = opponent_class
         self.player_decision    = player_decision
         self.opponent_decision  = opponent_decision
         self.player_strategy    = player_strategy
